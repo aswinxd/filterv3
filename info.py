5fc8d95e9049faf3fc6a473c77b6be4790da816f
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-12 11:29:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,8 +50,8 @@ REQ_CHANNEL = (int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001916031725'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Tom_Support')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002049466903'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'subotsupport')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
